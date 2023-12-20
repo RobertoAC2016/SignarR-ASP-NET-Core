95b2cc1b8b431b34e81d728334076ad97ab4153d
Roberto Avila <ravila@dealergeek.com>
2023-12-19 18:38:15 -0600

    Primer commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignarR_ASP_NET_Core.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(String user, String Message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, Message);
+        }
+    }
+}
